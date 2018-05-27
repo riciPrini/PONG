a25f19cdbab5b979384eb9422646e34d12847d07
riciPrini <35687888+riciPrini@users.noreply.github.com>
2018-05-27 09:51:19 +0200

    Add Gameplay

--- FerriPong/Form1.Designer.cs
+++ FerriPong/Form1.Designer.cs
@@ -29,11 +29,69 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.plain = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.plain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plain
+            // 
+            this.plain.BackColor = System.Drawing.Color.Black;
+            this.plain.Location = new System.Drawing.Point(157, 366);
+            this.plain.Name = "plain";
+            this.plain.Size = new System.Drawing.Size(168, 15);
+            this.plain.TabIndex = 0;
+            this.plain.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Lime;
+            this.ball.Location = new System.Drawing.Point(133, 205);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(22, 23);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.ball);
+            this.panel.Controls.Add(this.plain);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(478, 393);
+            this.panel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 393);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.plain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox plain;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel;
     }
 }
 
--- FerriPong/Form1.cs
+++ FerriPong/Form1.cs
@@ -12,9 +12,57 @@ namespace FerriPong
 {
     public partial class Form1 : Form
     {
+        public int speed_ball = 20;  
+        public int speed_top = 4;
+        public int point = 0;
         public Form1()
         {
             InitializeComponent();
+
+            timer1.Enabled = true;
+            Cursor.Hide();   
+            this.FormBorderStyle = FormBorderStyle.None;  
+            this.TopMost = true;  
+            this.Bounds = Screen.PrimaryScreen.Bounds; 
+            plain.Top = panel.Bottom - (panel.Bottom / 10);
+        }
+        
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+           if (e.KeyCode == Keys.Escape) { this.Close();}        
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            plain.Left = Cursor.Position.X - (plain.Width / 2);   
+            ball.Left += speed_ball;     //move the ball
+            ball.Top += speed_top;
+
+            if (ball.Bottom >= plain.Top && ball.Bottom <= plain.Bottom && ball.Left >= plain.Left && ball.Right <= plain.Right)    //collision
+            {
+                speed_top += 2;
+                speed_ball += 2;
+                speed_top = -speed_top;// change the direction
+                point += 1;
+            }
+            if (ball.Left <= panel.Left)
+            {
+                speed_ball = -speed_ball;
+            }
+            if (ball.Right >= panel.Right)
+            {
+                speed_ball = -speed_ball;
+            }
+            if (ball.Top <= panel.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if (ball.Bottom >= panel.Bottom)
+            {
+                timer1.Enabled = false; 
+            }
+
         }
     }
 }
